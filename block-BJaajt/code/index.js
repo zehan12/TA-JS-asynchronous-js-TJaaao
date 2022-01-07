218346d34693d1ed10ffb4137dcff16b0796d72d
Zehan Khan <zehan9211@gmail.com>
2022-01-07 16:16:27 +0530

    added followers

--- block-BJaajt/code/index.js
+++ block-BJaajt/code/index.js
@@ -1,18 +1,19 @@
-const img = document.querySelector("img");
+const img = document.querySelector(".profile");
 const userName = document.querySelector("h3");
 const company = document.querySelector("p");
 const followers = document.querySelector(".followers");
 const following = document.querySelector(".following");
 const input = document.querySelector("input");
-
+const foll = document.querySelector("foll");
+const body = document.querySelector('body');
 
 function displayUI(data){
     img.src = data.avatar_url;
     userName.innerText = data.name;
     company.innerText = data.company;
     followers.innerText = `Followers: ${data.followers}`;
-    following.innerText = `Following: ${data.following}`
-
+    following.innerText = `Following: ${data.following}`;
+            
 }
 
 function handelChange(event){
@@ -21,12 +22,35 @@ function handelChange(event){
         fKingXHR.open('GET',`https://api.github.com/users/${event.target.value}`);
         fKingXHR.onload = function(){
             let userData = JSON.parse(fKingXHR.response);
-            console.log(userData)
-            let arr = [...userData]
-            console.log(arr);
-            Object.keys(userData).forEach(( value )=>{ 
-                console.log( value.followers_url); 
-             });
+            var arr = userData
+
+            let fl = arr.followers_url;
+            console.log(fl)
+            
+        let fKingXHR1 = new XMLHttpRequest();
+            fKingXHR1.open('GET',`${fl}`);
+            fKingXHR1.onload = function(){
+                let userFollow = JSON.parse(fKingXHR1.response);
+                console.log(userFollow);
+                userFollow.forEach((e)=>{
+                    console.log(e.login)
+                    let rImg = document.createElement("img");
+                    rImg.classList.add("round");
+                    console.log(rImg)
+                    rImg.src = e.avatar_url;
+                    
+                    body.append(rImg);
+                })
+                
+        }
+            fKingXHR1.send();
+            
+            // arr.forEach((e)=>{
+            //     console.log(e)
+            // })
+            // Object.keys(userData).forEach(( value )=>{ 
+            //     console.log( value.followers_url); 
+            //  });
             displayUI(userData);
         };
         fKingXHR.onerror = function(){
@@ -34,6 +58,8 @@ function handelChange(event){
         };
         fKingXHR.send();
         event.target.value = '';
+        
+        
     }
 }
 
