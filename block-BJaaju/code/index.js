2728b1e3e305de12d6aee960ca9cc9417e4c09b4
Zehan Khan <zehan9211@gmail.com>
2022-01-08 15:18:14 +0530

    fix error

--- block-BJaaju/code/index.js
+++ block-BJaaju/code/index.js
@@ -1,31 +1,44 @@
 
-const img = document.querySelector("img");
+// const img = document.querySelector("img");
 const input = document.querySelector("input");
 const body = document.querySelector("body");
-img.style.height = "400px";
-img.style.width = "400px";
+let div = document.createElement("div")
+    div.style.display = "flex";
+    div.style.flexDirection = "row";
+    div.style.flexWrap = "wrap";
+    div.style.justifyContent = "space-evenly";
+    body.append(div);
 
 
 
 function handelChange(event){
     if (event.keyCode === 13 ){
     let fKingXHR = new XMLHttpRequest();
-
-fKingXHR.open('GET',`https://api.unsplash.com/search/photos?query=${event.target.value}&client_id=ilGa2_Ql0bDszy8XXGlQ8RMxdLT5ZyfU4FFx0AbtM3c`);
+    img.src.length = 0
+    let keyWord = event.target.value.toLowerCase();
+fKingXHR.open('GET',`https://api.unsplash.com/search/photos?query=${keyWord}&client_id=ilGa2_Ql0bDszy8XXGlQ8RMxdLT5ZyfU4FFx0AbtM3c`);
 
 fKingXHR.onload = function () {
     let imgData = JSON.parse(fKingXHR.response);
     console.log(imgData)
     data = [imgData];
+   
     console.log(data[0].results)
     let arr = [];
     data[0].results.forEach((ele) => {
-        console.log(ele.urls.regular);
-        arr.push(ele.urls.regular)   
+        console.log(ele.urls.small);
+        arr.push(ele.urls.small);   
     });
     arr.forEach((v)=>{
+        img.src = " ";
+        let box = document.createElement("div");
+        box.style.padding = "30px";
+        div.append(box);
+        let img = document.createElement("img");
+        img.style.height = "300px";
+        img.style.width = "300px";
         img.src = v;
-        body.append(img);
+        box.append(img);
     })
     console.log(arr);
 };
@@ -40,6 +53,7 @@ fKingXHR.onerror = function () {
 }
 
 input.addEventListener('keyup',handelChange);
+console.log("script is working!!!")
 
 // api key
 // ilGa2_Ql0bDszy8XXGlQ8RMxdLT5ZyfU4FFx0AbtM3c
